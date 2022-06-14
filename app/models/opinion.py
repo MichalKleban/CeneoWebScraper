6d1708aef828934a719348a35192efd4fc9d2b42
MichalKleban <michal0kleban@gmail.com>
2022-06-14 14:48:14 +0200

    Homework 14.06.2022 MK

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,32 +1,44 @@
-from app import parameters
+from app.parameters import selectors
 from app.utils import get_item
-
 class Opinion():
-    def __init__(self, opinion_id="", author="", recommendation=None, stars=0, content="", pros=[], cons=[], useful=0, useless=0, published_date=None, purchased_date=None):
-        self.opinion_id=opinion_id
-        self.author=author
-        self.recommendation=recommendation
-        self.stars=stars
-        self.content=content
-        self.pros=pros
-        self.cons=cons
-        self.useful=useful
-        self.useless=useless
-        self.published_date=published_date
-        self.purchased_date=purchased_date
+    def __init__(self, author="", recommendation=None, stars=0, content="", pros=[], cons=[], useful=0, useless=0, publish_date=None, purchase_date=None,  opinion_id=""):
+        self.opinion_id = opinion_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.pros = pros
+        self.cons = cons
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
         return self
     
     def extract_opinion(self, opinion):
         for key, value in selectors.items():
             setattr(self, key, get_item(opinion, *value))
-        self.opinion_id = opinion["data-entry-id"]             
+        self.opinion_id = opinion["data-entry-id"]
         return self
     
     def __str__(self) -> str:
-        pass
-
+        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
+    
     def __repr__(self) -> str:
-        pass
+        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
 
-    def to_dict(self):
-        pass
+    def to_dict(self) -> dict:
+        opinion_data ={
+            "author" : self.author,
+            "recommendation" : self.recommendation,
+            "stars" : self.stars, 
+            "content" : self.content,
+            "useful" : self.useful,
+            "useless" : self.useless,
+            "publish_date" : self.publish_date,
+            "purchase_date" : self.purchase_date,
+            "pros" : self.pros,
+            "cons" : self.cons,
+            "opinion_id" : self.opinion_id
+        }
+        return opinion_data
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,24 @@
 from app import app
 from flask import render_template, redirect, url_for, request
-import requests
 import json
-from bs4 import BeautifulSoup
 import os
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
 from app.models.product import Product
 
 @app.route('/')
 def index():
     return render_template("index.html.jinja")
 
-@app.route('/extract', methods=["GET","POST"])
+@app.route('/extract', methods=["POST", "GET"])
 def extract():
     if request.method == "POST":
         product_id = request.form.get("product_id")
         product = Product(product_id)
         product.extract_name()
-        
         if product.product_name:
-            product.extract_opinions()
+            product.extract_opinions().calculate_stats().draw_charts()
         else:
             pass
 
-        if not os.path.exists("app/opinions"):
-            os.mkdir("app/opinions")
-        
-        with open(f"app/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
-            json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
         return redirect(url_for('product', product_id=product_id))
     else:
         return render_template("extract.html.jinja")
@@ -45,6 +34,5 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-
-    return render_template("product.html.jinja", product_id=product_id, stats=stats, opinion=opinions)
-
+    
+    return render_template("product.html.jinja", product_id=product_id, stats=stats, opinions=opinions)
